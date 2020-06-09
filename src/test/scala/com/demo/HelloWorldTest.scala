128982233d8acc6768d33dbd198cffa6d71fb385
Michel Daviot <michel@daviot.info>
2020-06-09 12:43:50 +0200

    trying to provide a matcher on NES

--- src/test/scala/com/demo/HelloWorldTest.scala
+++ src/test/scala/com/demo/HelloWorldTest.scala
@@ -1,29 +1,40 @@
 package com.demo
 
+import com.demo.Util.anyNes
 import eu.timepit.refined.auto._
 import eu.timepit.refined.types.string.NonEmptyString
+import org.mockito.ArgumentMatcher
+import org.mockito.matchers.AnyMatcher
 import org.mockito.scalatest.MockitoSugar
 import org.scalatest.Matchers
 import org.scalatest.flatspec.AnyFlatSpecLike
 
 class HelloWorldTest extends AnyFlatSpecLike with MockitoSugar with Matchers {
-  val hello = mock[HelloWorld]
-  val helloRefined = mock[RefinedHelloWorld]
 
   "greeter" should "greet" in {
+    val hello = mock[HelloWorld]
     when(hello.greet(*)) thenReturn "hello"
     hello.greet("me") shouldBe "hello"
   }
 
   "refined" should "match and greet" in {
+    val helloRefined = mock[RefinedHelloWorld]
     when(helloRefined.greet("me")) thenReturn "hello"
     when(helloRefined.greet("you")) thenReturn "bye"
     helloRefined.greet("me") shouldBe "hello"
     helloRefined.greet("you") shouldBe "bye"
   }
 
-//  "refined" should "greet" in {
-//    when(helloRefined.greet(*[NonEmptyString])) thenReturn "hello"
-//    hello.greet("me") shouldBe "hello"
-//  }
+  "refined" should "greet" in {
+    val helloRefined = mock[RefinedHelloWorld]
+    when(helloRefined.greet(anyNes.any)) thenReturn "hello"
+    helloRefined.greet("me") shouldBe "hello"
+    helloRefined.greet("you") shouldBe "hello"
+  }
+}
+
+object Util {
+  def anyNes: AnyMatcher[NonEmptyString] =new AnyMatcher[NonEmptyString]{
+    override def any: NonEmptyString = NonEmptyString(" ")
+  }
 }
