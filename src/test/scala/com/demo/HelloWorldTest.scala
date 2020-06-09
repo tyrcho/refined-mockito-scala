1a4905397ee2879010927aac931fcee026f9301c
Michel Daviot <michel@daviot.info>
2020-06-09 12:15:31 +0200

    workaround with comparison to actual value

--- src/test/scala/com/demo/HelloWorldTest.scala
+++ src/test/scala/com/demo/HelloWorldTest.scala
@@ -1,5 +1,7 @@
 package com.demo
 
+import eu.timepit.refined.auto._
+import eu.timepit.refined.types.string.NonEmptyString
 import org.mockito.scalatest.MockitoSugar
 import org.scalatest.Matchers
 import org.scalatest.flatspec.AnyFlatSpecLike
@@ -13,8 +15,15 @@ class HelloWorldTest extends AnyFlatSpecLike with MockitoSugar with Matchers {
     hello.greet("me") shouldBe "hello"
   }
 
-  "refined" should "greet" in {
-    when(helloRefined.greet(*)) thenReturn "hello"
-    hello.greet("me") shouldBe "hello"
+  "refined" should "match and greet" in {
+    when(helloRefined.greet(NonEmptyString("me"))) thenReturn "hello"
+    when(helloRefined.greet(NonEmptyString("you"))) thenReturn "bye"
+    helloRefined.greet("me") shouldBe "hello"
+    helloRefined.greet("you") shouldBe "bye"
   }
+
+//  "refined" should "greet" in {
+//    when(helloRefined.greet(*[NonEmptyString])) thenReturn "hello"
+//    hello.greet("me") shouldBe "hello"
+//  }
 }
